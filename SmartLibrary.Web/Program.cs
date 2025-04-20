6208c1fb143e9f4523f1408f3831383bcf21b04f
Vlad Afanasev <vlad.afanasev@logius.nl>
2025-04-20 16:56:43 +0200

    Updated Program.cs to match the MVC pattern

--- SmartLibrary.Web/Program.cs
+++ SmartLibrary.Web/Program.cs
@@ -1,37 +1,44 @@
-using SmartLibrary.Web.Components;
 using SmartLibrary.Infrastructure.Data;
 using SmartLibrary.Application.Interfaces;
+using SmartLibrary.Application.Services;
 using SmartLibrary.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+// Add services to the container
+builder.Services.AddControllersWithViews();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddLogging();
+
+// Database configuration
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 if (string.IsNullOrEmpty(connectionString))
 {
     throw new InvalidOperationException("Connection string 'DefaultConnection' is not configured.");
 }
 
+// Register services
 builder.Services.AddSingleton<ApplicationDbContext>(provider => new ApplicationDbContext(connectionString));
 builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IUserService, UserService>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
-app.UseAntiforgery();
+app.UseRouting();
+app.UseAuthorization();
 
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+// Configure routes
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
